2acd8d6b81b60345e058db5d4eb74f11ca9526a4
Sam Spoerl <spoerlsam@gmail.com>
2025-06-30 09:34:10 -0400

    Upgrade Tailwind CSS to v4 via `npx @tailwindcss/upgrade`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -202,7 +202,7 @@ function Photos() {
           <div
             key={image.src}
             className={clsx(
-              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
+              'relative aspect-9/10 w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800',
               rotations[imageIndex % rotations.length]
             )}
           >
